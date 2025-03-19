ba0af41d2bd67d907a609d168b950de67811dd2c
agus <33077305+agusc01@users.noreply.github.com>
2025-03-18 21:29:59 -0300

    Changed extension name on scripts

--- config.def.h
+++ config.def.h
@@ -3,14 +3,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Bar dbar[] = {
     //Icon  Command Update     Interval   Update Signal
-    {"",    SC("brightness.sh"),    10,             1  },
-    {"",    SC("volume.sh"),        10,             1  },
-    {"",    SC("thermal.sh"),       10,             0  },
-    {"",    SC("cpu.sh"),            5,             0  },
-    {"",    SC("memory.sh"),         5,             0  },
-    {"",    SC("wifi.sh"),          60,             0  },
-    {"",    SC("date.sh"),          50,             0  },
-    {"",    SC("battery.sh"),       60,             0  },
+    {"",    SC("brightness"),    10,             1  },
+    {"",    SC("volume"),        10,             1  },
+    {"",    SC("thermal"),       10,             0  },
+    {"",    SC("cpu"),            5,             0  },
+    {"",    SC("memory"),         5,             0  },
+    {"",    SC("wifi"),          60,             0  },
+    {"",    SC("date"),          50,             0  },
+    {"",    SC("battery"),       60,             0  },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
