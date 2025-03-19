99fdc1c0ff0df156523a6ff943cc52885ad73662
agus <33077305+agusc01@users.noreply.github.com>
2025-03-19 18:27:44 -0300

    New icons, remove comments, change order

--- config.def.h
+++ config.def.h
@@ -2,17 +2,18 @@
 
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Bar dbar[] = {
+    // TODO: use Update Signal
     //Icon  Command Update     Interval   Update Signal
-    {"",    SC("brightness"),    10,             1  },
-    {"",    SC("volume"),        10,             1  },
+    {"",    SC("brightness"),    10,             0  },
+    {"",    SC("volume"),        10,             0  },
     {"",    SC("thermal"),       10,             0  },
     {"",    SC("cpu"),            5,             0  },
     {"",    SC("memory"),         5,             0  },
-    {"",    SC("wifi"),          60,             0  },
     {"",    SC("date"),          50,             0  },
+    {"",    SC("wifi"),          60,             0  },
     {"",    SC("battery"),       60,             0  },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
-static unsigned int delimLen = 3;
+static char delim[] = "  ";
+static unsigned int delimLen = 2;
