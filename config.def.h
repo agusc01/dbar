74498d0985fc10c1c73f208abf34f22bcf9a772b
agus <33077305+agusc01@users.noreply.github.com>
2025-04-06 10:24:25 -0300

    No using signal or time in brightness & sound

--- config.def.h
+++ config.def.h
@@ -2,11 +2,11 @@
 
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Bar dbar[] = {
-    // TODO: use Update Signal
+    // TODO: use Update Signal. For now brightness & sound use refreshdbar function of fish
     //Icon  Command Update     Interval   Update Signal
     {"",    SC("wifi"),          60,             0  },
-    {"",    SC("brightness"),    10,             0  },
-    {"",    SC("volume"),        10,             0  },
+    {"",    SC("brightness"),     0,             0  },
+    {"",    SC("volume"),         0,             0  },
     {"",    SC("thermal"),       10,             0  },
     {"",    SC("cpu"),            5,             0  },
     {"",    SC("memory"),         5,             0  },
